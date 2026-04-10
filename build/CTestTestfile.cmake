3034e669eb7721ae56040a761155bb041ad239cb
ukanda <ukanda@hotmail.com>
2026-04-09 22:00:33 -0400

    cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/upendrakanda/dev/cpp/src/orderbook
-# Build directory: /Users/upendrakanda/dev/cpp/src/orderbook/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/upendrakanda/dev/cpp/src/orderbook/build/orderbook_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
